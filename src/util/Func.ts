d1df7d3002257fb6351e129044c62a25dc2eee82
FujiHaruka <fujiharuka@realglobe.jp>
2019-02-18 00:13:24 +0900

    Converter from api url to html url

--- src/util/Func.ts
+++ src/util/Func.ts
@@ -1,5 +1,7 @@
 import { PlainObject } from '../types/General'
 import moment from 'moment'
+import { GitHubResponse } from '../types/GitHubResponse'
+import PathParser from 'path-parser'
 
 export const mapObjKey = (callbackfn: (key: string) => string) => (
   obj: PlainObject,
@@ -41,4 +43,49 @@ const reviver = function(key: string, value: any) {
 export const TypedJSON = {
   stringify: (value: any) => JSON.stringify(value, replacer),
   parse: (text: string) => JSON.parse(text, reviver),
+}
+
+const pathParsers = {
+  pullRequestApi: new PathParser(
+    'https://api.github.com/repos/:owner/:repo/pulls/:number',
+  ),
+  pullRequestHtml: new PathParser(
+    'https://github.com/:owner/:repo/pulls/:number',
+  ),
+  issueApi: new PathParser(
+    'https://api.github.com/repos/:owner/:repo/issues/:number',
+  ),
+  issueHtml: new PathParser(
+    'https://github.com/:owner/:repo/issues/:number', //
+  ),
+}
+export const findHtmlUrl = (
+  notification: GitHubResponse.Notification,
+): string => {
+  const { type, url } = notification.subject
+  const repositoryUrl = notification.repository.html_url // マッチしないとき用の URL
+  switch (type) {
+    case 'PullRequest': {
+      const matched = pathParsers.pullRequestApi.test(url)
+      if (!matched) {
+        console.error(
+          `Something wrong on finding HTML URL. Type is "PullRequest", but given URL is ${url}`,
+        )
+        return repositoryUrl
+      }
+      return pathParsers.pullRequestHtml.build(matched)
+    }
+    case 'Issue': {
+      const matched = pathParsers.issueApi.test(url)
+      if (!matched) {
+        console.error(
+          `Something wrong on finding HTML URL. Type is "Issue", but given URL is ${url}`,
+        )
+        return repositoryUrl
+      }
+      return pathParsers.issueHtml.build(matched)
+    }
+    default:
+      return repositoryUrl
+  }
 }
